60eaace4d4a8551e5de2c5e74fd6cbd9b98746c9
rrueda3 <bhdyaqbh@mozmail.com>
2025-07-07 01:37:40 +0200

    adding files

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config():
     SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(24))
-    SQLALCHEMY_DATABASE_URI = os.getenv('Database_URL','')
+    SQLALCHEMY_DATABASE_URI = os.getenv('Database_URL','postgresql://neondb_owner:npg_hDAix4fsk9ZV@ep-empty-sky-a2yjgk3q-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BOOTSTRAP_BOOTSWATCH_THEME = 'journal'
